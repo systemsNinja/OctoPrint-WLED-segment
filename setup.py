12cb770a937251a4aa992146c23c7804b823402e
ewwwbananas <mkollek@gmail.com>
2025-01-21 13:16:23 -0500

    updated setup.py with my info

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ except:
 import versioneer
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "wled"
+plugin_identifier = "wled-segment"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_wled"
+plugin_package = "octoprint_wled-segment"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-WLED"
+plugin_name = "OctoPrint-WLED-segment"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = versioneer.get_version()
@@ -28,16 +28,16 @@ plugin_cmdclass = versioneer.get_cmdclass()
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """Connect and control your WLED devices through OctoPrint"""
+plugin_description = """Connect and control your WLED devices through OctoPrint withSegment control"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Charlie Powell"
 
 # The plugin's author's mail address.
-plugin_author_email = "cp2004.github@gmail.com"
+plugin_author_email = "ewwwbananas@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/cp2004/OctoPrint-WLED"
+plugin_url = "https://github.com/systemsNinja/OctoPrint-WLED-segment"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
